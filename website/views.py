50f4e005de8a9c5750b0f0a0e45f9abf16d2dcb0
Mahmudul Haque Raihan <70860980+mhraihan@users.noreply.github.com>
2023-06-10 00:23:46 +0600

    login and logout features

--- website/views.py
+++ website/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
+
 
 # Create your views here.
+def home(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # authticase
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You've been logged in successfully")
+            return redirect("home")
+        else:
+            messages.warning(request, "There was an error, please try again later")
+            return redirect("home")
+    else:
+        return render(request, "home.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "User logged out successfully.")
+    return redirect("home")
